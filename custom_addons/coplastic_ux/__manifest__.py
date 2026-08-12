eca9c3da852a3eb251e5e2b1803cd116304ef163
root <root@srv898392.hstgr.cloud>
2026-08-12 16:03:05 +0000

    coplastic_ux: ajout de données de configuration
    Ajoute le répertoire data/ et le déclare au manifest.

--- custom_addons/coplastic_ux/__manifest__.py
+++ custom_addons/coplastic_ux/__manifest__.py
@@ -4,12 +4,15 @@
     'category': 'Technical',
     'summary': 'Améliorations UX pour Coplastic (navigation Enter dans les listes)',
     'author': 'Coplastic',
-    'depends': ['web', 'account'],
+    'depends': ['web', 'account', 'sales_team'],
     'assets': {
         'web.assets_backend': [
             'coplastic_ux/static/src/js/list_enter_navigation.js',
         ],
     },
+    'data': [
+        'data/menu_access.xml',
+    ],
     'installable': True,
     'auto_install': False,
     'application': False,
